5b594986418164e77eff51c82407e57373ca9ce4
Derek <derekdanielson84@gmail.com>
2023-07-25 11:21:59 -0600

    Added count function

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const btns = document.querySelectorAll('.btn');
 
 btns.forEach(function(btn){
     btn.addEventListener('click', function(e){
-        console.log(e.currentTarget);
+        const styles = e.currentTarget.classList;
+        if (styles.contains('decrease')){
+            count--;
+        } else if (styles.contains('increase')){
+            count++;
+        } else {
+            count = 0;
+        }
+        value.textContent = count;
     });
 });
