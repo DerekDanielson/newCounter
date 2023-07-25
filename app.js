30f2128e66c97a2a4d51e9e6087bfa8f9023ddbd
Derek <derekdanielson84@gmail.com>
2023-07-25 11:14:30 -0600

    Adding event listeners

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 // Set initial count
 let count = 0;
+
+// Select value and buttons
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach(function(btn){
+    btn.addEventListener('click', function(e){
+        console.log(e.currentTarget);
+    });
+});
